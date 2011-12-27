c9799fe9ebf2b60473ef3ef72dc2cf017583ab5d
Kasun Herath <kasun@thinkcube.com>
2011-12-28 02:12:06 +0530

    Three clients are instantiated at module run time

--- clients.py
+++ clients.py
@@ -15,24 +15,69 @@
 # Source - https://github.com/kasun/
 
 import threading
+from random import choice
+
+import zmq
 
 class Client(threading.Thread):
-    ''' Represents an example client '''
+    ''' Represents an example client. '''
     def __init__(self, identity):
         threading.Thread.__init__(self)
         self.identity = identity
+        self.zmq_context = zmq.Context()
 
     def run(self):
+        ''' Main Execution. '''
         num1, num2 = self.generate_numbers()
+        print('Client ID - %s. Numbers to be added - %s and %s' % (self.identity, num1, num2))
+        socket = self.get_connection()
+        
+        # Poller is used to check for availability of data to be read through the socket.
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
+        self.send(socket, '%s:%s' % (num1, num2))
+
+        # Infinitely poll for the result. 
+        # Polling is used to check for sockets with data before reading because socket.recv() is blocking.
+        while True:
+            # Poll for 5 seconds. Return any sockets with data to be read.
+            sockets = dict(poller.poll(5000))
+
+            # If socket has data to be read.
+            if socket in sockets and sockets[socket] == zmq.POLLIN:
+                result = self.receive(socket)
+                print('Client ID - %s. Numbers sent to be added - %s and %s. Received result - %s' % (self.identity, num1, num2, result))
+                break
+
+        socket.close()
+        self.zmq_context.term()
 
     def send(self, socket, data):
-        pass
+        ''' Send data through provided socket. '''
+        socket.send(data)
 
     def receive(self, socket):
-        pass 
+        ''' Recieve and return data through provided socket. '''
+        return socket.recv()
 
     def get_connection(self):
-        pass
+        ''' Create a zeromq socket of type DEALER; set it's identity, connect to server and return socket. '''
+
+        # Socket type DEALER is used in asynchronous request/reply patterns.
+        # It prepends identity of the socket with each message.
+        socket = self.zmq_context.socket(zmq.DEALER)
+        socket.setsockopt(zmq.IDENTITY, self.identity)
+        socket.connect('tcp://127.0.0.1:5001')
+        return socket
 
     def generate_numbers(self):
-        pass
+        ''' Generate and return a pair of numbers. '''
+        number_list = range(0,10)
+        num1 = choice(number_list)
+        num2 = choice(number_list)
+        return num1, num2
+
+if __name__ == '__main__':
+    for i in range(1,4):
+        client = Client(i)
+        client.start()
