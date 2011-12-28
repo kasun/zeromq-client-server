9fdb38a382e40ea8f5b27b7221555de3dd8b3169
Kasun Herath <kasun@thinkcube.com>
2011-12-29 01:43:39 +0530

    Server implemented.

--- server.py
+++ server.py
@@ -22,30 +22,50 @@ class Server(object):
         Instantiate workers, Accept client connections, distribute computation requests among workers and route computed results back to clients. '''
 
     def __init__(self):
-        pass
+        self.zmq_context = zmq.Context()
 
     def start(self):
-        pass
+        ''' Main execution. '''
+        # Front facing socket to accept client connections.
+        socket_front = self.zmq_context.socket(zmq.ROUTER)
+        socket_front.bind('tcp://127.0.0.1:5001')
+
+        # Backend socket to distribute work.
+        socket_back = self.zmq_context.socket(zmq.DEALER)
+        socket_back.bind('inproc://backend')
+
+        # Start three workers.
+        for i in range(1,4):
+            worker = Worker(self.zmq_context, i)
+            worker.start()
+
+        # Use built in queue device to distribute requests among workers.
+        zmq.device(zmq.QUEUE, socket_front, socket_back)
 
 class Worker(threading.Thread):
     ''' Workers accept computation requests from front facing server.
         Does computations and return results back to server. '''
 
-    def __init__(self):
+    def __init__(self, zmq_context, _id):
         threading.Thread.__init__(self)
-        self.zmq_context = zmq.Context()
+        self.worker_id = _id
+        self.zmq_context = zmq_context
 
     def run(self):
         ''' Main execution. '''
+        # Socket to communicate with front facing server.
         socket = self.zmq_context.socket(zmq.DEALER)
+        socket.connect('inproc://backend')
 
         while True:
-            # First string recieved is client ID
+            # First string recieved is socket ID of client
             client_id = socket.recv()
             request = socket.recv()
+            print('Worker ID - %s. Recieved computation request.' % (self.worker_id))
             result = self.compute(request)
+            print('Worker ID - %s. Sending computed result back.' % (self.worker_id))
 
-            # For successful route of result to correct client first the client ID should be sent
+            # For successful routing of result to correct client, the socket ID of client should be sent first.
             socket.send(client_id, zmq.SNDMORE)
             socket.send(result)
 
